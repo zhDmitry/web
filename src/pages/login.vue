49d33895b8eb02f1af3252801562acd473f00881
Andrii Lysenko <senkoily@gmail.com>
2017-10-08 18:59:15 +0300

    rewrite login page, change index.html title

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
     <h4>Вхід в особистий кабінет</h4>
-    <form>
-      <div class="form-group">
-        <label for="email">поштова скринька:</label>
-        <input type="email" class="form-control" id="email">
-      </div>
-      <div class="form-group">
-        <label for="pwd">пароль:</label>
-        <input type="password" class="form-control" id="pwd">
-      </div>
-      <button type="submit" class="btn btn-default">Увійти</button>
-    </form>
+    <v-form v-model="valid" ref="form" lazy-validation>
+      <v-text-field
+        label="поштова скринька"
+        v-model="email"
+        :rules="emailRules"
+        required
+      ></v-text-field>
+      <v-text-field
+        label="пароль"
+        :rules="nameRules"
+        type="password"
+        min="6"
+        required
+      ></v-text-field>
+      <v-select
+        label="роль"
+        v-model="select"
+        :items="['Студент','Викладач','Методист','Адміністратор']"
+        required
+      ></v-select>
+      <v-btn
+        @click="submit"
+      >увійти
+      </v-btn>
+    </v-form>
   </div>
 </template>
 
