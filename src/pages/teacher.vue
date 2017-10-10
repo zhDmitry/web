23258b72180fb656c9733d678655788316bd83ab
Andrii Lysenko <senkoily@gmail.com>
2017-10-10 23:37:57 +0300

    add Zhovtuhin's work

--- src/pages/teacher.vue
+++ src/pages/teacher.vue
@@ -1,11 +1,45 @@
 <template>
-    <div>
-        teacher
-    </div>
+    <v-card>
+        <v-card-title>
+            Викладачі
+            <v-spacer></v-spacer>
+            <v-text-field append-icon="search" label="Search" single-line hide-details v-model="search"></v-text-field>
+        </v-card-title>
+        <v-data-table v-bind:headers="headers" v-bind:items="items" v-bind:search="search">
+            <template slot="items" scope="props">
+                <td class="text-xs-right" @click="navigate(props.item.id)">{{ props.item.name }}</td>
+                <td class="text-xs-right">{{ props.item.university }}</td>
+                <td class="text-xs-right">{{ props.item.faculty }}</td>
+                <td class="text-xs-right">{{ props.item.department }}</td>
+            </template>
+            <template slot="pageText" scope="{ pageStart, pageStop }">
+                From {{ pageStart }} to {{ pageStop }}
+            </template>
+        </v-data-table>
+    </v-card>
 </template>
 
 <script>
 export default {
-    name: 'teacher'
+    data() {
+        return {
+            max25chars: (v) => v.length <= 25 || 'Input too long!',
+            tmp: '',
+            search: '',
+            pagination: {},
+            headers: [
+                { text: 'ПІБ', value: 'name' },
+                { text: 'Університет', value: 'university' },
+                { text: 'Факультет', value: 'faculty' },
+                { text: 'Кафедра', value: 'department' }
+            ],
+            items: require('../../mock/teacher.json')
+        }
+    },
+    methods: {
+        navigate(to) {
+            this.$router.push('/teacher/' + to)
+        },
+    }
 }
 </script>
