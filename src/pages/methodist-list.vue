03402389ba7163e29765c1bb7d7ade2f8161665b
Vlad Zagorulko <zagorulko@users.noreply.github.com>
2017-10-12 06:49:52 +0300

    Add user creation dialogs

--- src/pages/methodist-list.vue
+++ src/pages/methodist-list.vue
@@ -1,10 +1,61 @@
 <template>
-    <div>
-        methodist
-    </div>
+  <div>
+    <v-card>
+      <v-card-title>
+        <span class="h5 m-0">Методисти</span>
+        <v-spacer></v-spacer>
+        <v-text-field append-icon="search" label="Пошук" single-line hide-details v-model="search"></v-text-field>
+      </v-card-title>
+      <v-data-table v-bind:headers="headers" v-bind:items="items" v-bind:search="search">
+        <template slot="items" scope="props">
+          <td class="text-xs-right">{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.university }}</td>
+          <td class="text-xs-right">{{ props.item.faculty }}</td>
+          <td class="text-xs-right">{{ props.item.department}}</td>
+        </template>
+        <template slot="pageText" scope="{ pageStart, pageStop }">
+          From {{ pageStart }} to {{ pageStop }}
+        </template>
+      </v-data-table>
+      <v-card-actions>
+        <v-dialog v-model="dialog" persistent width="500px">
+          <v-btn color="primary" dark slot="activator">Зареєструвати методиста</v-btn>
+          <v-card>
+            <v-card-text>
+              <v-text-field label="ПІБ"></v-text-field>
+              <v-text-field label="Університет"></v-text-field>
+              <v-text-field label="Факультет"></v-text-field>
+              <v-text-field label="Кафедра"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="black" flat @click.native="dialog=false">Скасувати</v-btn>
+              <v-btn color="blue darken-1" flat @click.native="dialog=false">Зберегти</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      max25chars: (v) => v.length <= 25 || 'Input too long!',
+      tmp: '',
+      search: '',
+      pagination: {},
+      headers: [
+        { text: 'ПІБ', value: 'name' },
+        { text: 'Університет', value: 'university' },
+        { text: 'Факультет', value: 'faculty' },
+        { text: 'Кафедра', value: 'department' }
+      ],
+      items: require('../../mock/teacher.json'),
+      dialog: false
+    }
+  }
 }
 </script>
--- src/pages/student-list.vue
+++ src/pages/student-list.vue
@@ -1,47 +1,65 @@
 <template>
+  <div>
     <v-card>
-        <v-card-title>
-            Студенти
-            <v-spacer></v-spacer>
-            <v-text-field append-icon="search" label="Search" single-line hide-details v-model="search"></v-text-field>
-        </v-card-title>
-        <v-data-table v-bind:headers="headers" v-bind:items="items" v-bind:search="search">
-            <template slot="items" scope="props">
-                <td class="text-xs-right" @click="navigate(props.item.id)">{{ props.item.name }}</td>
-                <td class="text-xs-right">{{ props.item.university }}</td>
-                <td class="text-xs-right">{{ props.item.faculty }}</td>
-                <td class="text-xs-right">{{ props.item.speciality }}</td>
-                <td class="text-xs-right">{{ props.item.year }}</td>
-            </template>
-            <template slot="pageText" scope="{ pageStart, pageStop }">
-                From {{ pageStart }} to {{ pageStop }}
-            </template>
-        </v-data-table>
+      <v-card-title>
+        <span class="h5 m-0">Студенти</span>
+        <v-spacer></v-spacer>
+        <v-text-field append-icon="search" label="Пошук" single-line hide-details v-model="search"></v-text-field>
+      </v-card-title>
+      <v-data-table v-bind:headers="headers" v-bind:items="items" v-bind:search="search">
+        <template slot="items" scope="props">
+          <td class="text-xs-right">{{ props.item.name }}</td>
+          <td class="text-xs-right">{{ props.item.university }}</td>
+          <td class="text-xs-right">{{ props.item.faculty }}</td>
+          <td class="text-xs-right">{{ props.item.speciality }}</td>
+          <td class="text-xs-right">{{ props.item.year }}</td>
+        </template>
+        <template slot="pageText" scope="{ pageStart, pageStop }">
+          From {{ pageStart }} to {{ pageStop }}
+        </template>
+      </v-data-table>
+      <v-card-actions v-if="canCreate">
+        <v-dialog v-model="dialog" persistent width="500px">
+          <v-btn color="primary" dark slot="activator">Зареєструвати студента</v-btn>
+          <v-card>
+            <v-card-text>
+              <v-text-field label="ПІБ"></v-text-field>
+              <v-text-field label="Університет"></v-text-field>
+              <v-text-field label="Факультет"></v-text-field>
+              <v-text-field label="Спеціальність"></v-text-field>
+              <v-text-field label="Курс"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="black" flat @click.native="dialog=false">Скасувати</v-btn>
+              <v-btn color="blue darken-1" flat @click.native="dialog=false">Зберегти</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-card-actions>
     </v-card>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            max25chars: (v) => v.length <= 25 || 'Input too long!',
-            tmp: '',
-            search: '',
-            pagination: {},
-            headers: [
-                { text: 'ПІБ', value: 'name' },
-                { text: 'Університет', value: 'university' },
-                { text: 'Факультет', value: 'faculty' },
-                { text: 'Спеціальність', value: 'speciality' },
-                { text: 'Курс', value: 'year' },
-            ],
-            items: require('../../mock/student.json')
-        }
-    },
-    methods: {
-        navigate(to) {
-            this.$router.push('/student/' + to)
-        },
+  data() {
+    return {
+      max25chars: (v) => v.length <= 25 || 'Input too long!',
+      tmp: '',
+      search: '',
+      pagination: {},
+      headers: [
+        { text: 'ПІБ', value: 'name' },
+        { text: 'Університет', value: 'university' },
+        { text: 'Факультет', value: 'faculty' },
+        { text: 'Спеціальність', value: 'speciality' },
+        { text: 'Курс', value: 'year' },
+      ],
+      items: require('../../mock/student.json'),
+      dialog: false,
+      canCreate: ['admin', 'methodist'].includes(this.$store.state.role)
     }
+  }
 }
 </script>
